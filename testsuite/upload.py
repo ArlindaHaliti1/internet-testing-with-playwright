72c2f5cf5f2c439b174567b8c3937ce7bb79b545
ArlindaHaliti1 <arlindahaliti55@gmail.com>
2025-04-17 15:53:26 +0200

    Update upload.py

--- testsuite/upload.py
+++ testsuite/upload.py
@@ -11,11 +11,11 @@ def file_upload_page(page):
 
 def test_upload_valid_file(file_upload_page):
     file_upload_page.upload_file(
-        'C:/Users/ArlindaHaliti/OneDrive - Telos-Labs/Desktop/internet-testing-with-playwright/utilities/playwright.txt'
+        './utilities/playwright.txt'
     )
     file_upload_page.submit()
     file_upload_page.assert_upload_success('playwright.txt')
 
-def test_upload_without_file(file_upload_page):
+def test_upload_without_file(file_upload_page): 
     file_upload_page.submit()  
     file_upload_page.assert_upload_failure()
